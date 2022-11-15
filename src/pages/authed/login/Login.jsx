3964ec2f7f121d408a501addd6f325a2c3c59d82
jibola2588 <jibolafawole258@gmail.com>
2022-11-15 15:59:36 -0800

    added toggle functionality to password input

--- src/pages/authed/login/Login.jsx
+++ src/pages/authed/login/Login.jsx
@@ -1,11 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext,useState,useRef,} from 'react';
 import { Wrapper } from './Login.styles';
 import { Link } from 'react-router-dom';
 import Logo from '../../../assets/Vertical_RGB_294.svg'
-
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
 
 
  const Login = () => {
+
+  const [viewPassword, setViewPassword] = useState(true);
+  const numberInput = useRef(null);
+
+
+  const togglePasswordView = () => {
+    setViewPassword(!viewPassword);
+    viewPassword == true
+      ? (numberInput.current.type = "text")
+      : (numberInput.current.type = "password");
+  };
+
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const [error,setError] = useState('')
+  const [success,setSuccess] = useState('')
+
   return (
     <Wrapper>
       <section className=" dark:bg-gray-900">
@@ -22,14 +39,43 @@ import Logo from '../../../assets/Vertical_RGB_294.svg'
               </h1>
 
               <p className='text-center text-[16px] text-[#667085] font-[400] mt-[0]' style={{ marginTop: '0' }}>Welcome back! Please enter your details.</p>
-              <form className="space-y-4 md:space-y-6" action="#">
+              <form className="space-y-4 md:space-y-6">
                 <div>
                   <label for="email" className="block mb-2 text-sm font-medium text-[#344054] dark:text-white">Email</label>
-                  <input type="email" name="email" id="email" className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email" required />
+                  <input 
+                  type="email" 
+                  name="email" 
+                  id="email" 
+                  className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                  placeholder="Enter your email" 
+                  required />
                 </div>
-                <div>
+                <div className='relative'>
                   <label for="password" className="block mb-2 text-sm font-medium text-[#344054] normal dark:text-white">Password</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••" className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                  <input 
+                  type={numberInput} 
+                  name="password" 
+                  id="password" 
+                  placeholder="Enter your password"
+                   ref={numberInput}
+                  className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                  required />
+
+                   <span class="absolute  inline-flex items-center right-4 top-[55%]">
+                <div className="flex justify-end">
+                  {viewPassword ? (
+                    < FaEye 
+                    style = {{width:22, height:20,color:'#344054'}}
+                    onClick={ togglePasswordView } />
+            
+                  ) : (
+                    < FaEyeSlash 
+                    style = {{ width:22, height:20,color:'#344054'}}
+                    onClick={ togglePasswordView }  />
+                  )
+                  }       
+                   </div>
+              </span>
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
@@ -43,7 +89,10 @@ import Logo from '../../../assets/Vertical_RGB_294.svg'
                   <Link to='/forgottonPassword' className="text-sm font-medium text-[#3068AE] hover:underline dark:text-primary-500">Forgot password?</Link>
                 </div>
             
-                <Link to='/verifyEmail' type="submit" className="w-full text-white bg-[#3068AE] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 outline-none">Sign in</Link>
+                <button 
+                 type="submit" 
+                 className="w-full text-white bg-[#3068AE] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 outline-none"
+                 >Sign in</button>
               
               </form>
             </div>
